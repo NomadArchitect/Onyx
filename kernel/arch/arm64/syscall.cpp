6fbb4d0b3352f16e5a821ba48f12be296b008bfe
Pedro Falcato <pedro.falcato@gmail.com>
2023-04-25 04:07:47 +0100

    arm64: Add vfork, pipe2
    Also fix sigaltstack on arm64 and riscv64. Oops.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/arch/arm64/syscall.cpp
+++ kernel/arch/arm64/syscall.cpp
@@ -27,12 +27,12 @@ long do_syscall64(registers_t *frame)
     unsigned long syscall_nr = frame->x[8];
 
     /* In case of a fork or sigreturn, adjust %rdi so it points to the frame */
-    if (syscall_nr == SYS_fork || syscall_nr == SYS_sigreturn /* || syscall_nr == SYS_vfork*/)
+    if (syscall_nr == SYS_fork || syscall_nr == SYS_sigreturn || syscall_nr == SYS_vfork)
         frame->x[0] = (unsigned long) frame;
 
     /* sigaltstack's implementation requires the syscall frame as the 3rd argument */
     if (syscall_nr == SYS_sigaltstack)
-        frame->x[0] = (unsigned long) frame;
+        frame->x[2] = (unsigned long) frame;
 
     long ret = 0;
 
